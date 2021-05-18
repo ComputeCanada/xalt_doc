87cc5a1c1444df877966238bd1b1678dbd8837af
Justin Lague <julag27@ulaval.ca>
2021-05-18 13:22:08 -0400

    Ruby filter refactor

---
+++ elk/logstash/xalt_filter.rb
@@ -0,0 +1,19 @@
+to_keep = ['@timestamp', '@version', 'libA', 'type']
+
+def filter(event)
+    flatten_libA(event)
+    remove_fields(event)
+end
+
+def flatten_libA(event)
+    event.set('libA', event.get('libA').map! {|elem| elem[0]})
+    return event
+end
+
+def remove_fields(event)
+    event.to_hash.each_key do |key|
+        if not to_keep.include?(key)
+            event.remove(key)
+        end
+    end
+end
