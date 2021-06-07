9eca51ec5eb735b3bd0f619a62017ec3f19ab4ec
Justin Lague <julag27@ulaval.ca>
2021-06-07 17:50:17 -0400

    Add logstash finder function

--- config/logstash/logstash.sh
+++ config/logstash/logstash.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
-logstash -f $1 \
+function find_logstash {
+    cmd=$(which logstash)
+    BIN=/usr/share/logstash/bin/logstash
+    if [ -z "$cmd" ]; then
+        if [ -f $BIN ]; then
+            cmd=$BIN
+        else
+            echo "The logstash executable was not found" >&2
+        fi
+    fi
+    echo $cmd
+}
+
+CMD=$(find_logstash)
+
+$CMD -f $1 \
     --path.settings   /etc/logstash   \
     --path.logs       ./logs          \
     --path.data       ./data          \
-    --log.level       debug           \
-    --config.reload.automatic
+    --log.level       debug           
+    #--config.reload.automatic
